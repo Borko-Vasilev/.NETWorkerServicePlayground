6a9e91b121d84c2c0aa47135d11d42de688911b8
Borislav Vasilev <102370371+Borko-Vasilev@users.noreply.github.com>
2022-11-09 16:23:31 +0200

    Adding Domain objects

---
+++ src/InitialWorkerService.Domain/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace InitialWorkerService.Domain
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> Get(
+           Expression<Func<T, bool>> filter = null,
+           Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+           string includeProperties = "");
+
+        Task<IEnumerable<T>> GetAll(string includeProperties = "");
+
+        Task<T> GetById(int id);
+
+        Task<T> GetById(Expression<Func<T, bool>> filter, string includeProperties = "");
+
+        Task Update(T entityToUpdate);
+
+        Task Add(T entity);
+
+        Task Delete(T entity);
+
+        Task Delete(object id);
+    }
+}
